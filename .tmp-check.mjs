02161cc340f8e8bdb19b559b1329b9f49121695a
Lockou <kaiojeffoficial@gmail.com>
2026-02-21 03:21:31 -0300

    Remove

--- .tmp-check.mjs
+++
@@ -1,4 +0,0 @@
-﻿import { pathToFileURL } from 'node:url';
-await import(pathToFileURL('Commands/Utilidades/devex.ts').href);
-await import(pathToFileURL('index.ts').href);
-console.log('imports ok');
--- Dockerfile
+++
@@ -1,3 +0,0 @@
-RUN apt-get update && apt-get install -y python3-pip
-RUN pip3 install requests
-RUN pip install pyotp
